f3f8775247afc4ce5722a702b92fb400bc236f03
makefiles <1mak2r3@gmail.com>
2024-01-27 14:07:52 +0900

    백테스트에서 그래프 표시 추가

--- backtest_vb_ma.py
+++ backtest_vb_ma.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pyupbit
 import yaml
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as patches
 
 # 설정 읽기
 with open('config.yaml', encoding='UTF-8') as f:
@@ -12,7 +16,7 @@ upbit = pyupbit.Upbit(_cfg['UPBIT_ACCESS'], _cfg['UPBIT_SECRET'])
 class BackTestVbMa:
     def __init__(self, config):
         # 일봉 데이터
-        self.df = pyupbit.get_ohlcv(config["TICKER"], count=config["DAYS"]).reset_index()
+        self.df = pyupbit.get_ohlcv(config["TICKER"], count=config["DAYS"]).reset_index(names='datetime')
 
         # 슬리피지 + 업비트 매도/매수 수수료 (0.05% * 2)
         self.fee = config["SLIPPAGE"] + config["COMMISSION"]
@@ -32,17 +36,17 @@ class BackTestVbMa:
         df['target_price'] = df['open'] + df['range'] * self.k
 
         # 이동 평균선 교차 전략 (Moving average crossover)
-        df['ma5'] = df['close'].rolling(5).mean()
         df['ma10'] = df['close'].rolling(10).mean()
+        df['ma20'] = df['close'].rolling(20).mean()
 
         # N/A 행 제거
-        df = df.dropna().reset_index(drop=True)
-        # print(df.to_string())
+        df = df.dropna().reset_index(drop=True, names='datetime')
+        print(df.to_string())
 
         # 매수 신호 (목표 금액에 달성한 경우 목표 금액에 매수해 다음날 시가에 매도한 것으로 판단)
         df['buy'] = np.where((df['low'] < df['target_price']) \
                              & (df['target_price'] < df['high']) \
-                             & (df['ma10'] < df['ma5']), 1, 0)
+                             & (df['ma20'] < df['ma10']), 1, 0)
 
         # 수익률 계산 (다음날 시가와 당일 종가의 시간 차이가 거의 없어 종가로 계산)
         df['ror'] = np.where(df['buy'] == 1, df['close'] / df['target_price'] - self.fee, 1)
@@ -62,6 +66,7 @@ class BackTestVbMa:
 
         # 결과 출력
         self.print(df)
+        self.show(df)
 
     def print(self, df):
         """ 결과 출력 """
@@ -77,13 +82,52 @@ class BackTestVbMa:
         print('최저 잔액 : %s' % df['cash'].min())
         print('최대 낙폭 (MDD) : %s' % df['dd'].max())
         print('=' * 40)
+        
+    def show(self, df):
+        """ 그래프 표시 """
+        # plot setting
+        plt.figure(figsize = (20, 10))
+        sns.set_style('whitegrid')
+
+        # get data
+        coin_y = df['open'].values
+        buy_df = df[df['buy']==1]
+        buy_x = buy_df.index.values
+        sell_x = buy_df.index + 1
+
+        # plot data
+        plt.plot(coin_y, c = 'gray', label='coin price', linewidth=1.2)
+        plt.scatter(buy_x, coin_y[buy_x], c='lime', s=15, label='buy/sell point')
+        plt.scatter(sell_x, coin_y[sell_x], c='lime', s=15)
+        for idx in range(0, len(buy_x)):
+            rec_xs = [buy_x[idx], sell_x[idx]]
+            rec_ys = [coin_y[buy_x[idx]], coin_y[sell_x[idx]]]
+            if rec_ys[0] < rec_ys[1]:
+                rec_color = 'blue'
+            else:
+                rec_ys[0], rec_ys[1] = rec_ys[1], rec_ys[0]
+                rec_color = 'red'
+            plt.gca().add_patch(patches.Rectangle((rec_xs[0], rec_ys[0]), 
+                                                rec_xs[1]-rec_xs[0], 
+                                                rec_ys[1]-rec_ys[0],
+                                                alpha = 0.4,
+                                                color = rec_color))
+        # legend
+        plt.gca().add_patch(patches.Rectangle((0, 0), 0, 0, color = 'blue', label = 'benefit'))
+        plt.gca().add_patch(patches.Rectangle((0, 0), 0, 0, color = 'red', label = 'loss'))
+        plt.legend()
+
+        # plot setting
+        plt.ylim(min(coin_y)*0.95, max(coin_y)*1.05)
+        plt.xticks(list(range(0, len(df), 1)), labels = df['datetime'][list(range(0, len(df), 1))], rotation = 45)
+        plt.show()
 
 
 BackTestVbMa({
     "TICKER": "KRW-BTC",
-    "DAYS": 160,
+    "DAYS": 100,
     "CASH": 1000000,
     "SLIPPAGE": 0.002,
     "COMMISSION": 0.001,
-    "K": 0.5
+    "K": 0.3
 }).test()
--- backtest_vb_prophet.py
+++ backtest_vb_prophet.py
@@ -3,6 +3,10 @@ import numpy as np
 import pyupbit
 import yaml
 import logging
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as patches
 
 logging.getLogger("prophet").setLevel(logging.WARNING)
 logging.getLogger("cmdstanpy").disabled = True
@@ -26,11 +30,11 @@ class BackTestVbProphet:
         self.study_time = config["STUDY_DAYS"] * 24
 
         # 일봉 데이터
-        self.daily_df = pyupbit.get_ohlcv(config["TICKER"], count=config["TEST_DAYS"]).reset_index()
+        self.daily_df = pyupbit.get_ohlcv(config["TICKER"], count=config["TEST_DAYS"]).reset_index(names='datetime')
 
         # 시봉 데이터
         self.hourly_df = (pyupbit.get_ohlcv(config["TICKER"], interval="minute60",
-                                            count=self.test_days * 24 + self.study_time).reset_index())
+                                            count=self.test_days * 24 + self.study_time).reset_index(names='datetime'))
 
         # 슬리피지 + 업비트 매도/매수 수수료 (0.05% * 2)
         self.fee = config["SLIPPAGE"] + config["COMMISSION"]
@@ -51,13 +55,14 @@ class BackTestVbProphet:
 
         # 15일 데이터 시계열 예측 전략 (Prophet)
         # print(df.to_string())
-        df['predict'] = df.apply(lambda row: self.predict_price(row.name, row['index']), axis=1)
+        df['predict_price'] = df.apply(lambda row: self.predict_price(row.name, row['datetime']), axis=1)
+        # df['predict_price'] = df['target_price'] + 1
         print('\r')
 
         # 매수 신호 (목표 금액에 달성한 경우 목표 금액에 매수해 다음날 시가에 매도한 것으로 판단)
         df['buy'] = np.where((df['low'] < df['target_price']) \
                              & (df['target_price'] < df['high']) \
-                             & (df['target_price'] < df['predict']), 1, 0)
+                             & (df['target_price'] < df['predict_price']), 1, 0)
 
         # 수익률 계산 (다음날 시가와 당일 종가의 시간 차이가 거의 없어 종가로 계산)
         df['ror'] = np.where(df['buy'] == 1,
@@ -78,6 +83,7 @@ class BackTestVbProphet:
 
         # 결과 출력
         self.print(df)
+        self.show(df)
 
     def print(self, df):
         """ 결과 출력 """
@@ -104,8 +110,8 @@ class BackTestVbProphet:
     def predict_price(self, index, date):
         """ Prophet 시계열 예측 가격 """
         df = self.hourly_df
-        df = df[df['index'] <= date].iloc[-self.study_time:]  # date 이전 15일 시봉
-        data_df = df[['index', 'close']].rename(columns={'index': 'ds', 'close': 'y'})
+        df = df[df['datetime'] <= date].iloc[-self.study_time:]  # date 이전 15일 시봉
+        data_df = df[['datetime', 'close']].rename(columns={'datetime': 'ds', 'close': 'y'})
         model = Prophet()
         model.fit(data_df)
         future = model.make_future_dataframe(periods=24, freq='H')
@@ -116,13 +122,65 @@ class BackTestVbProphet:
         close_value = close_df['yhat'].values[0]
         self.progress_bar(index + 1, self.test_days)
         return close_value
+    
+    def show(self, df):
+        """ 그래프 표시 """
+        # plot setting
+        plt.figure(figsize = (20, 10))
+        sns.set_style('whitegrid')
+
+        # get data
+        coin_y = df['open'].values
+        buy_df = df[df['buy']==1]
+        buy_x = buy_df.index.values
+        sell_x = buy_df.index + 1
+        buy_yhat = buy_df['predict_price'].values
+        if coin_y.shape[0] == sell_x[-1]:
+            buy_x = buy_x[:-1]
+            sell_x = sell_x[:-1]
+
+        # plot data
+        plt.plot(coin_y, c = 'gray', label='coin price', linewidth = 1.2)
+        plt.scatter(buy_x, coin_y[buy_x], c='lime', s=15, label='buy/sell point')
+        plt.scatter(sell_x, coin_y[sell_x], c='lime', s=15)
+        for idx in range(0, len(buy_x)):
+            rec_xs = [buy_x[idx], sell_x[idx]]
+            rec_ys = [coin_y[buy_x[idx]], coin_y[sell_x[idx]]]
+            if rec_ys[0] < rec_ys[1]:
+                rec_color = 'blue'
+            else:
+                rec_ys[0], rec_ys[1] = rec_ys[1], rec_ys[0]
+                rec_color = 'red'
+                
+            plt.plot([buy_x[idx], sell_x[idx]], [buy_yhat[idx]] * 2, c = 'orange')
+            plt.vlines(x = buy_x[idx], 
+                    ymin = min(coin_y)*0.95,
+                    ymax = buy_yhat[idx], 
+                    linestyle = '--',
+                    linewidth = 0.5,
+                    color = 'orange')
+            plt.gca().add_patch(patches.Rectangle((rec_xs[0], rec_ys[0]), 
+                                                rec_xs[1] - rec_xs[0], 
+                                                rec_ys[1] - rec_ys[0],
+                                                alpha = 0.4,
+                                                color = rec_color))
+        # legend
+        plt.plot([0,0], c='orange', label='prediction')
+        plt.gca().add_patch(patches.Rectangle((0, 0), 0, 0, color = 'blue', label = 'benefit'))
+        plt.gca().add_patch(patches.Rectangle((0, 0), 0, 0, color = 'red', label = 'loss'))
+        plt.legend()
+
+        # plot setting
+        plt.ylim(min(coin_y) * 0.95, max(coin_y) * 1.05)
+        plt.xticks(list(range(0, len(df), 1)), labels = df['datetime'][list(range(0, len(df), 1))], rotation = 45) 
+        plt.show()
 
 BackTestVbProphet({
     "TICKER": "KRW-BTC",
-    "TEST_DAYS": 160,
-    "STUDY_DAYS": 15,
+    "TEST_DAYS": 100,
+    "STUDY_DAYS": 20,
     "CASH": 1000000,
     "SLIPPAGE": 0.002,
     "COMMISSION": 0.001,
-    "K": 0.5
+    "K": 0.3
 }).test()
